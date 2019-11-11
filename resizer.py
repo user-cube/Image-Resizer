00e490349a6e1e807b503de7502fb16a257da146
Rui <33549232+user-cube@users.noreply.github.com>
2019-11-11 20:46:52 +0000

    Create resizer.py

---
+++ resizer.py
@@ -0,0 +1,83 @@
+from PIL import Image
+import os
+import argparse
+
+def resizeImages():
+    if not os.path.isdir(OUT):
+        os.mkdir(OUT)
+    
+    if SINGLE == 0:
+        isDir = False
+    else:
+        isDir = True
+        entries = os.listdir(CONTENT)
+    
+    if isDir:
+        for i in entries:
+            imageFile = i
+            im1 = Image.open(CONTENT + imageFile)
+            isOk = False
+
+            if FILTER == 1:
+                im2 = im1.resize((WIDTH, HEIGHT), Image.NEAREST)
+                isOk = True
+            if FILTER == 2:
+                im2 = im1.resize((WIDTH, HEIGHT), Image.BILINEAR)
+                isOk = True
+            if FILTER == 3:
+                im2 = im1.resize((WIDTH, HEIGHT), Image.BICUBIC)
+                isOk = True
+            if FILTER == 4:
+                im2 = im1.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
+                isOk = True
+
+            if isOk :             
+                if EXT == '.jpg':
+                    im2 =  im2.convert("RGB")
+
+                im2.save(OUT + i.split(".")[0] + EXT)
+    else:
+        if not os.path.isdir(OUT):
+            os.mkdir(OUT)
+
+        im1 = Image.open(CONTENT)
+        if FILTER == 1:
+            im2 = im1.resize((WIDTH, HEIGHT), Image.NEAREST)
+            isOk = True
+        if FILTER == 2:
+            im2 = im1.resize((WIDTH, HEIGHT), Image.BILINEAR)
+            isOk = True
+        if FILTER == 3:
+            im2 = im1.resize((WIDTH, HEIGHT), Image.BICUBIC)
+            isOk =  True
+        if FILTER == 4:
+            im2 = im1.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
+            isOk = True
+
+        if isOk :             
+            if EXT == '.jpg':
+                im2 =  im2.convert("RGB")
+
+            im2.save(OUT + i.split(".")[0] + EXT)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--single", help="Only converts a simple image file", default=0)
+    parser.add_argument("--filter", help="1- NEAREST\n2-BILINEAR\n3-BICUBIC\n4-ANTIALIAS", default=1)
+    parser.add_argument("--content", help="File or directory to convert", default="myimage.png")
+    parser.add_argument("--out", help="Output directory", default="resized/")
+    parser.add_argument("--h", help="HEIGHT", default=500)
+    parser.add_argument("--w", help="WIDTH", default=500)
+    parser.add_argument("--ext", help="Output extension file", default=".png")
+
+    args = parser.parse_args()
+    SINGLE = int(args.single)
+    FILTER = int(args.filter)
+    CONTENT = args.content
+    OUT = args.out
+    HEIGHT = int(args.h)
+    WIDTH = int(args.w)
+    EXT = args.ext
+    
+    resizeImages()
